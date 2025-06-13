99ebaaa5a86e77e55356cc39c93761ee8017ea4c
niranjana kannan <niranjana.k.2020@gmail.com>
2025-06-13 18:18:17 +0530

    Backend

---
+++ backend.py
@@ -0,0 +1,33 @@
+import requests
+import os
+
+api_key = os.getenv("PASSWORD")
+
+def get_data(place, days, kind):
+    
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
+
+    request = requests.get(url)
+    content = request.json()
+
+    if kind == "Temperature":
+        temperature = []
+        date = []
+        for i in range(8*days):
+            temperature.append(content["list"][i]["main"]["temp"])
+            date.append(content["list"][i]["dt_txt"])
+
+        return temperature, date
+    
+    if kind == "Sky":
+        sky = []
+        date = []
+
+        for i in range(8*days):
+            sky.append(content["list"][i]["weather"][0]["description"])
+            date.append(content["list"][i]["dt_txt"])
+
+        return sky, date
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("Place")
-
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted")
-
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"Temperature for the next {days} day(s) in {place.title()}")
